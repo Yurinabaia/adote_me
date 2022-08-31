36b44f313faeedca178ccc18b3a3a2e30497584b
yurinabaia <yurinabaiaduarte@gmail.com>
2022-08-30 23:26:25 -0300

    Aleracao na main

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,9 @@ import 'package:adoteme/ui/screens/create_publication/basic_animal_data_screen.d
 import 'package:adoteme/ui/screens/create_publication/select_publication_screen.dart';
 import 'package:adoteme/ui/screens/create_publication/steps_create_publication.dart';
 import 'package:adoteme/ui/screens/login_screen.dart';
-import 'package:adoteme/ui/screens/my_publications_screen.dart';
 import 'package:adoteme/ui/screens/user_profile_screen.dart';
 import 'package:adoteme/utils/theme_data.dart';
 import 'package:adoteme/ui/screens/first_access_screen.dart';
-import 'package:adoteme/ui/screens/splash_screen.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
