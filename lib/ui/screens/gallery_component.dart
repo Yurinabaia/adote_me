90dd6dedd3824ab3cc9be3c9d3adae3efac96422
lucasluan01 <lucas.l.santos0204@gmail.com>
2022-09-06 15:31:40 -0300

    Implementação da visualização das imagens em tela cheia

---
+++ lib/ui/screens/gallery_component.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+
+class GalleryComponent extends StatefulWidget {
+  final int initialIndex;
+  final List<String?> galleryItems;
+
+  const GalleryComponent({
+    super.key,
+    this.initialIndex = 0,
+    required this.galleryItems,
+  });
+
+  @override
+  State<GalleryComponent> createState() => _GalleryComponentState();
+}
+
+class _GalleryComponentState extends State<GalleryComponent> {
+  late int currentIndex = widget.initialIndex;
+
+  void onPageChanged(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.galleryItems.every((element) => element != null)
+        ? Scaffold(
+            body: Stack(
+              children: <Widget>[
+                PhotoViewGallery.builder(
+                  scrollPhysics: const BouncingScrollPhysics(),
+                  builder: (BuildContext context, int index) {
+                    return PhotoViewGalleryPageOptions(
+                      imageProvider: NetworkImage(widget.galleryItems[index]!),
+                      initialScale: PhotoViewComputedScale.contained,
+                      minScale: PhotoViewComputedScale.contained,
+                      maxScale: PhotoViewComputedScale.contained * 10,
+                      heroAttributes: PhotoViewHeroAttributes(tag: index),
+                    );
+                  },
+                  itemCount: widget.galleryItems.length,
+                  onPageChanged: onPageChanged,
+                  backgroundDecoration: const BoxDecoration(
+                    color: Colors.black,
+                  ),
+                ),
+                Align(
+                  alignment: const Alignment(0.9, 0.9),
+                  child: Container(
+                    padding: const EdgeInsets.all(20.0),
+                    child: Text(
+                      "${currentIndex + 1} / ${widget.galleryItems.length}",
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18.0,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        : const Scaffold(
+            body: Center(
+              child: Text('Nenhuma imagem encontrada'),
+            ),
+          );
+  }
+}
